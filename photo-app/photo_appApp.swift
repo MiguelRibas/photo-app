a76af4c9d1231237602192659c6702cfa5e8af2b
MiguelRibas <mwilkerribas@gmail.com>
2025-02-03 09:02:17 -0400

    Initial Commit

---
+++ photo-app/photo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  photo_appApp.swift
+//  photo-app
+//
+//  Created by miguelribas on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct photo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
